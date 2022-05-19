4defbe60f070ad441951e1f2e78b9d2740f32b6a
Lucas Shinzoaki <https://github.com/lucasshinozaki>
2022-05-19 15:43:24 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'calculadora';
+  txt:string = ""
+  input: string | undefined;
+
+  somar(n1:number, n2:number){
+    this.txt ="Resultado = " + (n1 + n2);
+  }
+
+  sub(n1:number, n2:number){
+    this.txt ="Resultado = " + (n1 - n2);
+  }
+
+  mult(n1:number, n2:number): void{
+    this.txt ="Resultado = " + (n1 * n2);
+  }
+
+  div(n1:number, n2:number){
+    this.txt ="Resultado = " + (n1 / n2);
+  }
+ 
 }
